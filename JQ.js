d7b6f3ce7589e9ecf5ec91b8d7a2b8ed1fb9a152
onera <charmSmartProgramming@gmail.com>
2023-12-28 17:17:11 +0800

    done but self-spinning

---
+++ JQ.js
@@ -0,0 +1,23 @@
+var radius = 200; // adjust to move out items in and out 
+var fields = $('.field'),
+  container = $('#container'),
+  width = container.width(),
+  height = container.height();
+var angle = 0,
+  step = (2 * Math.PI) / fields.length;
+fields.each(function() {
+  var x = Math.round(width / 2 + radius * Math.cos(angle) - $(this).width() / 2);
+  var y = Math.round(height / 2 + radius * Math.sin(angle) - $(this).height() / 2);
+  if (window.console) {
+    console.log($(this).text(), x, y);
+  }
+  $(this).css({
+    left: x + 'px',
+    top: y + 'px'
+  });
+  angle += step;
+});
+
+// $('button').click(function change() {
+//     $('.field').fadeToggle();
+// })
