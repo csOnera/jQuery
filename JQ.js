10d69911c1d7e70db1603d2906483e098afc18ed
onera <charmSmartProgramming@gmail.com>
2023-12-29 16:53:06 +0800

    accomplished? added zoom in and out with opacity

--- JQ.js
+++ JQ.js
@@ -1,22 +1,65 @@
-var radius = 200; // adjust to move out items in and out 
-var fields = $('.field'),
-  container = $('#container'),
-  width = container.width(),
-  height = container.height();
-var angle = 0,
-  step = (2 * Math.PI) / fields.length;
-fields.each(function() {
-  var x = Math.round(width / 2 + radius * Math.cos(angle) - $(this).width() / 2);
-  var y = Math.round(height / 2 + radius * Math.sin(angle) - $(this).height() / 2);
-  if (window.console) {
-    console.log($(this).text(), x, y);
-  }
-  $(this).css({
-    left: x + 'px',
-    top: y + 'px'
+$(document).ready(function(){
+  var radius = 10; // adjust to move out items in and out 
+  var fields = $('.field'),
+    container = $('#container'),
+    width = container.width(),
+    height = container.height();
+  var angle = 0,
+    step = (2 * Math.PI) / fields.length;
+  var y_scroll_pos = window.scrollY;
+  var scroll_pos = $("#container").offset().top;
+
+  const circular = function() {
+    var x = Math.round(width / 2 + radius * Math.cos(angle) - $(this).width() / 2);
+    var y = Math.round(height / 2 + radius * Math.sin(angle) - $(this).height() / 2);
+    // if (window.console) {
+    //   console.log($(this).text(), x, y);
+    // }
+    $(this).css({
+      left: x + 'px',
+      top: y + 'px'
+    });
+    angle += step;
+  };
+
+  const addRadiusByTime = setInterval(function(){
+    y_scroll_pos = window.scrollY;
+    scroll_pos = $("#container").offset().top;
+    if(radius<=200 && y_scroll_pos > scroll_pos) {
+      radius += 5
+      console.log("added 10")
+    };
+    fields.each(circular);
+  }, 40);
+
+  
+
+  $(window).on('scroll', function(){
+    y_scroll_pos = window.scrollY;
+    scroll_pos = $("#container").offset().top;
+    console.log(scroll_pos)
+    if(y_scroll_pos > scroll_pos) {
+      $(".container").addClass("active")
+      addRadiusByTime
+
+    } else {
+      radius = 10;
+      $(".container").removeClass("active")
+    }
+    
   });
-  angle += step;
-});
+
+  
+})
+
+// var controller = new ScrollMagic.Controller();
+
+// var scene = new ScrollMagic.Scene({
+//   triggerElement: "#container"
+// }).setTween("#container", "active")
+// .addTo(controller);
+
+
 
 // $('button').click(function change() {
 //     $('.field').fadeToggle();
